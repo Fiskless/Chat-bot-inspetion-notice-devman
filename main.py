958530731e4754a42f9bfc6b9189bf749ad15979
Fiskless <Fiskless@gmail.com>
2021-09-19 22:14:46 +0500

    Add nem bot logger

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from urllib.parse import urljoin
 
 class TelegramLogsHandler(logging.Handler):
 
-    def __init__(self, tg_bot, chat_id):
+    def __init__(self, token, chat_id):
         super().__init__()
         self.chat_id = chat_id
-        self.tg_bot = tg_bot
+        self.tg_bot = telegram.Bot(token=token)
 
     def emit(self, record):
         log_entry = self.format(record)
@@ -57,38 +57,40 @@ def send_message_using_bot(bot, chat_id, response):
 def main():
     load_dotenv()
 
-    TOKEN = os.getenv("TOKEN")
+    TOKEN_NOTIFICATION_BOT = os.getenv("TOKEN_NOTIFICATION_BOT")
     CHAT_ID = os.getenv("CHAT_ID")
     TOKEN_API_DEVMAN = os.getenv("TOKEN_API_DEVMAN")
+    TOKEN_LOGGER_BOT = os.getenv("TOKEN_LOGGER_BOT")
 
-    bot = telegram.Bot(token=TOKEN)
+    bot = telegram.Bot(token=TOKEN_NOTIFICATION_BOT)
     logger = logging.getLogger('tg_logger')
     logger.setLevel(logging.WARNING)
-    logger.addHandler(TelegramLogsHandler(bot, CHAT_ID))
+    logger.addHandler(TelegramLogsHandler(TOKEN_LOGGER_BOT, CHAT_ID))
     timestamp = None
     failed_connections = 0
-    while True:
-        try:
-            logger.warning("Бот запущен")
-            url = 'https://dvmn.org/api/long_polling/'
-            response = get_long_polling_review(
-                url,
-                TOKEN_API_DEVMAN,
-                timestamp
-            )
-            if response['status'] == 'found':
-                timestamp = response['last_attempt_timestamp']
-            else:
-                timestamp = response['timestamp_to_request']
-            send_message_using_bot(bot, CHAT_ID, response)
-        except requests.exceptions.ReadTimeout:
-            pass
-        except requests.exceptions.ConnectionError:
-            failed_connections += 1
-            if failed_connections % 5 == 0:
-                time.sleep(60)
-        except Exception:
-            logger.exception("Бот упал с ошибкой")
+    try:
+        logger.warning("Бот запущен")
+        while True:
+            try:
+                url = 'https://dvmn.org/api/long_polling/'
+                response = get_long_polling_review(
+                    url,
+                    TOKEN_API_DEVMAN,
+                    timestamp
+                )
+                if response['status'] == 'found':
+                    timestamp = response['last_attempt_timestamp']
+                else:
+                    timestamp = response['timestamp_to_request']
+                send_message_using_bot(bot, CHAT_ID, response)
+            except requests.exceptions.ReadTimeout:
+                pass
+            except requests.exceptions.ConnectionError:
+                failed_connections += 1
+                if failed_connections % 5 == 0:
+                    time.sleep(60)
+    except Exception:
+        logger.exception("Бот упал с ошибкой")
 
 
 if __name__ == '__main__':
